a39db4dc7a963267410aa6c28ab38fe4c3baabc1
ChrisRobb93 <41024073+ChrisRobb93@users.noreply.github.com>
2023-01-24 16:00:02 +0000

    Updated with params

--- Complete/Misc/Log_Cleanup.ps1
+++ Complete/Misc/Log_Cleanup.ps1
@@ -2,14 +2,18 @@
 Set this as a daily scheduled task to cleanup log files older than $DaysToKeep
 #>
 
-$DaysToKeep=14
-$LogDirectories="ENTERFULLPATHHERE"
+Param 
+(
+	    [Parameter(Mandatory=$True)]
+		[string] $DaysToKeep,
+
+		[Parameter(Mandatory=$True)]
+		[string] $LogDirectories,
+)
 
 
 Foreach ( $LogDirectory in $LogDirectories ) {
 
-	Get-ChildItem -path $LogDirectory -recurse |
-	Where-Object {$_.LastWriteTime -lt (get-date).AddDays(-$DaysToKeep) -and $_.Attributes -notlike "Directory" }|
-	Foreach-Object { Remove-Item $_.FullName }
+	Get-ChildItem -path $LogDirectory -recurse | Where-Object {$_.LastWriteTime -lt (get-date).AddDays(-$DaysToKeep) -and $_.Attributes -notlike "Directory" } | Remove-Item
 
 }
